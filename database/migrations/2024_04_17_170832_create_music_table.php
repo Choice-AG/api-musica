f0da9f789bd8a37bbc819547c29e53e0c0965bbb
Goizane <goizaneolaneta@gmail.com>
2024-05-15 00:16:59 +0200

    chore: Add album field to music table and controller

--- database/migrations/2024_04_17_170832_create_music_table.php
+++ database/migrations/2024_04_17_170832_create_music_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('artist');
+            $table->string('album');
             $table->string('genre');
             $table->string('year');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
